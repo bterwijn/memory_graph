db5e33b58452042419e80b144d0b34dac31b3a90
Bas Terwijn <bterwijn@gmail.com>
2023-05-20 09:30:57 +0200

    add filter() for show( filter(locals()) )

--- memory_graph/__init__.py
+++ memory_graph/__init__.py
@@ -27,63 +27,15 @@ def create(data):
     all_nodes=rewrite_to_node.rewrite_data(data)
     return grapviz_nodes.create_graph(all_nodes)
 
-def test():
-    blocking=True
-
-    #rewrite_to_node.reduce_references=False # don't reduce references
-    
-    class TestClass:
-        s1=1000
-        s2=[1,2,3]
-        def __init__(self):
-            self.a=99
-            self.b=(1,2)
-    data=[TestClass(),TestClass()]
-    show(data,block=blocking)
-    render(data,"graph.png",block=False)
-    
-    # test data
-    s1="hello world"
-    l1=[1,2,3,s1]
-
-    data=[l1]
-    #show(data,block=blocking)
-    #render(data,"test.pdf")
-    
-    t1=(10,20,30,40)
-    #data.append(t1)
-    #show(data,block=blocking)
-    d1={'k1':t1, 'k2':1234, 'k3':s1}
-    #data.append(d1)
-    #show(data,block=blocking)
-    class MyClass:
-        static1=1000
-        static2=t1
-        def __init__(self):
-            self.a=l1
-            self.b=t1
-            self.c=s1
-            self.d=d1
-        def set_ref(self,r):
-            self.ref=r
-    c1=MyClass()
-    show(c1,block=blocking)
-    
-    data.append(c1)
-    show(data,block=blocking)
-    data.append(10000088)
-    show(data,block=blocking)
-    
-    l1.append(data) # recursive
-    d1["k4"]=data
-    c1.set_ref(data)
-    #print(data)
-    show(data,block=blocking)
-
-    for i in range(10):
-        data.append(i)
-        show(data,block=blocking)
-
-    show(locals(),block=blocking) # locals
-    #show(Node.all_nodes,block) # all_nodes
-
+def filter(dictionary):
+    filtered_dict={}
+    for key in dictionary:
+        value=dictionary[key]
+        if rewrite.is_known_type(key) and rewrite.is_known_type(value):
+            if type(key)==str:
+                if rewrite.is_dunder_name(key):
+                    continue
+                if key in {'memory_graph'}:
+                    continue
+            filtered_dict[key]=value
+    return filtered_dict
