b88ba3e63e274580a1c96c721d54f91cf9107e04
Bas Terwijn <bterwijn@gmail.com>
2023-05-21 14:57:30 +0200

    using 'rank=same' trick to improve graph stability over different time steps

--- memory_graph/graphviz_nodes.py
+++ memory_graph/graphviz_nodes.py
@@ -11,7 +11,7 @@ type_category_to_color_map={
     "tuple":"orange", "list":"brown1", "set":"darkolivegreen1", "frozenset":"darkolivegreen3", "dict":"royalblue1", "mappingproxy":"royalblue3", "class":"orchid" # containers
 }
 uncategorized_color="red"
-padding=3
+padding=0
 spacing=3
 
 def type_category_to_color(type_catergory):
@@ -36,30 +36,27 @@ def add_escape_chars(label):
                                          "|":  r"\|",
                                          "{":  r"\{",
                                          "}":  r"\}",
-                                         }))
-    #if len(label)>0 and label[-1]=='>': # workaround, weird problem if label ends with '>'  # TODO
-    #    label+=" "
+                                         })) # TODO
     return label
 
 def get_element_label(element):
     value=element.get_value()
     if value is None:
         return "&nbsp;"
-    return str(value)
-    #return add_escape_chars(str(value)) # TODO vertical
+    return add_escape_chars(str(value))
 
-def build_label_line(node):
+def build_label_line(node,border=1):
     color=type_category_to_color(get_type_category(node))
     if len(node.get_elements())>0:
         if layout_vertical:
             cells="".join( (f'<TR><TD PORT="f{index}"> {get_element_label(element)} </TD></TR>' for index,element in enumerate(node.get_elements())) )
         else:
             cells="<TR>"+ "".join( (f'<TD PORT="f{index}">{get_element_label(element)}</TD>' for index,element in enumerate(node.get_elements())) ) +"</TR>"
-        table=f'<TABLE CELLSPACING="{spacing}" CELLPADDING="{padding}">{cells}</TABLE>'
+        table=f'<TABLE BORDER="{border}" CELLSPACING="{spacing}" CELLPADDING="{padding}">{cells}</TABLE>'
         return f'<<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" BGCOLOR="{color}"><TR><TD PORT="X"> {table} </TD></TR></TABLE>>'
     return f'<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0" BGCOLOR="{color}"><TR><TD PORT="X"> &nbsp; </TD></TR></TABLE>>'
     
-def build_label_key_value(node):
+def build_label_key_value(node,border=1):
     color=type_category_to_color(get_type_category(node))
     if len(node.get_elements())>0:
         iterator=iter(enumerate(node.get_elements()))
@@ -71,14 +68,14 @@ def build_label_key_value(node):
                 cells+=f'<TR><TD PORT="f{ki}"> {get_element_label(k)} </TD><TD PORT="f{vi}"> {get_element_label(v)} </TD></TR>'
             except StopIteration:
                 break
-            table=f'<TABLE CELLSPACING="{spacing}" CELLPADDING="{padding}" BGCOLOR="{color}">{cells}</TABLE>'
+            table=f'<TABLE BORDER="{border}" CELLSPACING="{spacing}" CELLPADDING="{padding}" BGCOLOR="{color}">{cells}</TABLE>'
         return f'<<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0"><TR><TD PORT="X"> {table} </TD></TR></TABLE>>'
     return f'<<TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="0" BGCOLOR="{color}"><TR><TD PORT="X"> &nbsp; </TD></TR></TABLE>>'
     
-def get_node_label(node):
+def get_node_label(node,border=1):
     if rewrite.is_dict_type(node.get_original_data()) or rewrite.is_type_with_dict(node.get_original_data()):
-        return build_label_key_value(node)
-    return build_label_line(node)
+        return build_label_key_value(node,border)
+    return build_label_line(node,border)
 
 def get_refs(node,all_nodes):
     return ((f"{get_node_name(node)}:f{index}",f"{get_node_name(all_nodes[element.get_ref()])}:X")
@@ -87,13 +84,18 @@ def get_refs(node,all_nodes):
 
 def create_node(node,all_nodes,graph):
     name="node"+str(node.get_index())
-    node_label=get_node_label(node)
+    border=2 if node.get_index()==0 else 1
+    node_label=get_node_label(node,border)
     type_name=get_type_name(node)
     color=type_category_to_color(get_type_category(node))
-    #if node.get_index()==0:
-    graph.node(name, node_label, xlabel=type_name) # TODO penwidth="3" 
+    graph.node(name, node_label, xlabel=type_name)
     for node_src,node_dst in get_refs(node,all_nodes):
-        graph.edge(node_src, node_dst)
+        graph.edge(node_src, node_dst, weight='0')
+    if rewrite.is_linear_type(node.get_original_data()):
+        children=[node_dst for node_src,node_dst in get_refs(node,all_nodes) ]
+        if len(children)>1:        
+            lineup=" -> ".join( children )
+            graph.body.append("{ rank=same  "+lineup+"  [weight=10,style=invis]; }\n") # dotted / invis
     
 def create_graph_recursive(all_nodes,node_index,memo,graph):
     if not node_index in memo:
