51bb061fb85336d1371d620d7d3859894445a2a3
Bas Terwijn <bterwijn@gmail.com>
2023-05-30 17:14:20 +0200

    printing filename,line,function of code that calls show()/render() when blocking

--- memory_graph/__init__.py
+++ memory_graph/__init__.py
@@ -3,8 +3,24 @@ from memory_graph import Node
 from memory_graph import rewrite_to_node
 from memory_graph import graphviz_nodes
 
+import traceback
+import inspect
+
 __version__ = "0.1.14"
 __author__ = 'Bas Terwijn'
+
+def block_print():
+    press="press <ENTER> to continue..."
+    try:
+        iterable=traceback.walk_stack(None)
+        iterator=iter(iterable)
+        next(iterator) # skip frame
+        frame,linenr=next(iterator) # read the frame that called show()/render()
+        tb=inspect.getframeinfo(frame)
+        return f'from file:"{tb.filename}" line:{tb.lineno} function:"{tb.function}", '+press
+    except:
+        pass
+    return s
 
 def create_graph(data):
     all_nodes=rewrite_to_node.rewrite_data(data)
@@ -14,15 +30,15 @@ def show(data,block=False):
     graph=create_graph(data)
     graph.view()
     if block:
-        input(f"showing '{graph.filename}', press <ENTER> to continue...")
+        input(f"showing '{graph.filename}', {block_print()}")
 
 def render(data,output_filename=None,block=False):
     graph=create_graph(data)
     if output_filename:
         graph.render(outfile=output_filename)
         if block:
-            input(f"rendering '{output_filename}', press <ENTER> to continue...")
+            input(f"rendering '{output_filename}', {block_print()}")
     else:
         graph.render()
         if block:
-            input(f"rendering '{graph.filename}', press <ENTER> to continue...")
+            input(f"rendering '{graph.filename}', {block_print()}")
