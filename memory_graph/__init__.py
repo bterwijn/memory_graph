fdf6de623920a5f39248d01107eafd67d6884423
Bas Terwijn <bterwijn@gmail.com>
2023-05-31 17:06:27 +0200

    minor update

--- memory_graph/__init__.py
+++ memory_graph/__init__.py
@@ -44,9 +44,10 @@ def render(data,output_filename=None,block=False,skipframes=1):
         if block:
             input(f"rendering '{graph.filename}', {block_print(skipframes)}")
 
-def d():
-    iterable=traceback.walk_stack(None)
-    iterator=iter(iterable)
-    frame,linenr=next(iterator) # read the frame that called show()/render()
-    locals_from_calling_frame=frame.f_locals
-    show(locals_from_calling_frame,block=True,skipframes=2)
+def d(data=None,block=True):
+    if data is None:
+        iterable=traceback.walk_stack(None)
+        iterator=iter(iterable)
+        frame,linenr=next(iterator) # read the frame that called d()
+        data=frame.f_locals # get locals() from the calling frame
+    show(data,block=block,skipframes=2)
