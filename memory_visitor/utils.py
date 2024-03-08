96542757ab747ddb8331a295ee640e3757f456f8
Bas Terwijn <bterwijn@gmail.com>
2024-03-08 22:22:28 +0100

    front_back_split(), front_back_repeat_split() added

--- memory_visitor/utils.py
+++ memory_visitor/utils.py
@@ -1,3 +1,6 @@
+import types
+import itertools
+import collections
 
 class class_type:
     pass
@@ -7,6 +10,13 @@ def ignore_exception(func):
         return func()
     except Exception as e:
         pass
+
+def does_work(func):
+    try:
+        func()
+        return True
+    except Exception as e:
+        return False
 
 def has_dict_attribute(value):
     return hasattr(value,"__dict__")
@@ -31,6 +41,39 @@ def is_self_iterating(data):
 def get_type_name(data):
     return type(data).__name__
 
+def front_back_split(iterable, front_size, back_size):
+    if isinstance(iterable, types.GeneratorType):
+        return (tuple(itertools.islice(iterable, front_size)), 
+                tuple(collections.deque(iterable, maxlen=back_size)))
+    else:
+        front = min(len(iterable), front_size)
+        back = max(len(iterable)-back_size, front)
+        return (iterable[:front], iterable[back:])
+
+def front_back_repeat_split(iterable, front_size, back_size, block_size):
+    if isinstance(iterable, types.GeneratorType):
+        while True:
+            block = tuple(itertools.islice(iterable, block_size))
+            if not block:
+                break
+            yield front_back_split(block, front_size, back_size)
+    else:
+        index=0
+        while (index < len(iterable)):
+            next_index = min(index + block_size, len(iterable))
+            front = min(len(iterable), index+front_size)
+            back = max(next_index - back_size, front)
+            yield (iterable[index:front], iterable[back:next_index])
+            index = next_index
+
 if __name__ == '__main__':
     ignore_exception(lambda x: 1/0)
+
+    n = 13
+    print( front_back_split([i for i in range(n)], 10, 5) )
+    print( front_back_split((i for i in range(n)), 10, 5) )
+
+    n = 15
+    print( tuple(front_back_repeat_split([i for i in range(n)], 4, 3, 10)) )
+    print( tuple(front_back_repeat_split((i for i in range(n)), 4, 3, 10)) )
 
