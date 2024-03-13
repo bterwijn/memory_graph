676d5f2e0338b34f35beef04f66f356ac02580fe
Bas Terwijn <bterwijn@gmail.com>
2024-03-13 12:13:09 +0100

    update

--- memory_visitor/Slider.py
+++ memory_visitor/Slider.py
@@ -9,7 +9,22 @@ def to_number(s):
             return None
 
 def limit_value(value,size):
-    return max(min(value,size-1),-size) 
+    return max(min(value,size-1),-size)
+
+def list_depth(data):
+    depth = 0
+    while isinstance(data,list):
+        depth += 1
+        if len(data) == 0:
+            break
+        data = data[0]
+    return depth
+
+def empty_list(depth):
+    data = []
+    for _ in range(depth-1):
+        data = [data]
+    return data
 
 class Slicer:
 
@@ -33,15 +48,38 @@ class Slicer:
             values = [None if v is None else 
                       limit_value(int(v*size) if isinstance(v,float) else v, size)
                       for v in s]
+            if values[2] == 0:
+                values[2] = 1
             yield slice(*values)
 
     def get_values(self,data):
+        if len(data) == 0:
+            return data
+        depth = list_depth(data)
+        first = True
+        last_slice = None
         for slice in self.get_slices(len(data)):
             print(slice)
+            if first:
+                first=False
+                if not (slice.start is None or slice.start==0):
+                    yield empty_list(depth)
             yield data[slice]
+            last_slice = slice
+        if not last_slice is None and not (last_slice.stop is None):
+            yield empty_list(depth)
 
 if __name__ == '__main__':
-    slicer = Slicer([":0.5:2","-0.7::3", "1.5:1.5:1.5", "-1.5:-1.5:-1.5"])
-    data = list(range(20))
-    for values in slicer.get_values(data):
-        print(values)
+
+    data = [[[[]]]]
+    print(empty_list(list_depth(data)))
+
+    slicer = Slicer(["1:0.5:2", "-0.7:-1:"])
+    n = 8
+    m = 3
+    data = [[i*10+j for j in range(m)] for i in range(n)]        
+    #data = [list(range(0,5)), list(range(5,10)), list(range(10,15))]
+    print('data:',data)
+    print(list(slicer.get_values(data)))
+    #for values in slicer.get_values(data):
+    #    print(values)
