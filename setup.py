448f25cb7d249aabcb7265e736d21eceb92fe311
Bas Terwijn <bterwijn@gmail.com>
2024-04-24 19:47:43 +0200

    version 0.2.05

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description_from_readme = (this_directory / "README.md").read_text()
 
 setup(
     name = 'memory_graph',
-    version = '0.2.04',
+    version = '0.2.05',
     description = 'Draws a graph of your data to analyze its structure.',
     long_description = long_description_from_readme,
     long_description_content_type = 'text/markdown',
