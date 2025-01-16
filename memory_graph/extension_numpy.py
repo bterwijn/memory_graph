762841c151ed9ad60049f676db1653cbcda44066
Bas Terwijn <bterwijn@gmail.com>
2025-01-16 01:33:25 +0100

    np.float_, np.complex_ removed

--- memory_graph/extension_numpy.py
+++ memory_graph/extension_numpy.py
@@ -8,8 +8,8 @@ import numpy as np
 
 config.not_node_types |= {
     np.int8, np.int16, np.int32, np.int64, np.uint8, np.uint16, np.uint32, np.uint64, 
-    np.float16, np.float32, np.float64, np.float_, 
-    np.complex64, np.complex128, np.complex_, 
+    np.float16, np.float32, np.float64,
+    np.complex64, np.complex128,
     np.bool_, np.bytes_, np.str_, np.datetime64, np.timedelta64
 }
 
