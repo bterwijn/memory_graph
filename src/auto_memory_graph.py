4c58ea6400a3ee4c5510c744f0bf0b2b2a5ef808
Bas Terwijn <bterwijn@gmail.com>
2025-02-02 16:11:22 +0100

    update

--- src/auto_memory_graph.py
+++ src/auto_memory_graph.py
@@ -8,9 +8,12 @@ def mg_visualization(execution_result):
     ipython_locals = get_ipython().user_ns
     mg.show(mg.ipython_locals_filter(ipython_locals))
 
-def mg_switch():
+def mg_switch(status = None):
     global mg_visualization_status
-    mg_visualization_status = not mg_visualization_status
+    if isinstance(status, bool):
+        mg_visualization_status = status
+    else:
+        mg_visualization_status = not mg_visualization_status
     if mg_visualization_status:
         get_ipython().events.register("post_run_cell", mg_visualization)
     else:
