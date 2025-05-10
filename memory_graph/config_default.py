df5d17e2efee7bef8b51c03e04b355721e3d4fb8
Bas Terwijn <bterwijn@gmail.com>
2025-05-10 11:28:01 +0200

    update

--- memory_graph/config_default.py
+++ memory_graph/config_default.py
@@ -82,7 +82,7 @@ config.type_to_color = {
     Node_Key_Value : "seagreen1", # for classes
     call_stack : 'khaki',
     type: "seagreen3",            # where class variables are stored
-    dict : "#80b3ff",
+    dict : "#66b3ff",
     types.MappingProxyType : "dodgerblue2", # not used
     range : "cornsilk2",
 }
