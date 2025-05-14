eadf65dc1050a368be05a604e504a06b10486ac9
Bas Terwijn <bterwijn@gmail.com>
2025-05-15 00:47:03 +0200

    update

--- images/linked_list.py
+++ images/linked_list.py
@@ -6,7 +6,7 @@ import memory_graph as mg
 import random
 random.seed(0) # use same random numbers each run
 
-class LinkedList:
+class Linked_List:
 
     def __init__(self, value=None, prev=None, next=None):
         self.prev = prev
@@ -17,15 +17,15 @@ class LinkedList:
         if self.value == None:
             self.value = value 
         elif self.next is None:
-            new_node = LinkedList(value)
+            new_node = Linked_List(value)
             self.prev = new_node
             self.next = new_node
         else:
-            new_node = LinkedList(value, self.next)
+            new_node = Linked_List(value, self.next)
             self.next.next = new_node
             self.next = new_node
 
-linked_list = LinkedList()
+linked_list = Linked_List()
 n = 100
 for i in range(n):
     value = random.randrange(n)
