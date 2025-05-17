cdc810b1ef21068c913eb0eae7f6e18538d01f9b
Bas Terwijn <bterwijn@gmail.com>
2025-05-17 16:29:01 +0200

    version 0.3.26

--- memory_graph/__init__.py
+++ memory_graph/__init__.py
@@ -14,7 +14,7 @@ from memory_graph.call_stack import call_stack
 
 import graphviz
 
-__version__ = "0.3.25"
+__version__ = "0.3.26"
 __author__ = 'Bas Terwijn'
 render_filename = 'memory_graph.pdf'
 render_filename_count = 0
