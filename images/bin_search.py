a4a40de5535a8709817985a8b399cf8ac939e899
Bas Terwijn <bterwijn@gmail.com>
2025-06-07 19:07:19 +0200

    bin_search added

---
+++ images/bin_search.py
@@ -0,0 +1,41 @@
+import memory_graph as mg
+import random
+random.seed(2) # same random numbers each run
+
+class View:
+
+    def __init__(self, lst, begin, end):
+        self.lst = lst
+        self.begin = begin
+        self.end = end
+
+    def __getitem__(self, index):
+        return self.lst[index]
+        
+    def get_mid(self):
+        return (self.begin + self.end) // 2
+
+mg.config.type_to_slicer[mg.node_linear.Node_Linear] = mg.slicer.Slicer()
+#mg.config.type_to_color[View] = 'hotpink'
+#mg.config.type_to_node[View] = lambda data: mg.node_linear.Node_Linear(data,
+#                                              data.lst[data.begin:data.end] )
+
+def bin_search(view, value):
+    mid = view.get_mid()
+    if view.begin == mid:
+        mg.render(mg.stack(), 'bin_search.png')
+        return view.begin
+    if value < view[mid]:
+        return bin_search(View(view.lst, view.begin, mid), value)
+    else:
+        return bin_search(View(view.lst, mid, view.end), value)
+
+# create sorted list
+n = 15
+data = [random.randrange(1000) for _ in range(n)]
+data.sort()
+
+# search 'value'
+value = data[random.randrange(n)]
+index = bin_search(View(data, 0, len(data)), value)
+print(f'{index=} {data[index]=}')
