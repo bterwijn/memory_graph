bterwijn/memory_graph

commit 108209c86dc50a5ed23c5d576f016fc31afc27b5

added some documentation, cleaning up code

memory_graph/memory_to_nodes.py
@@ -5,11 +5,13 @@
 from memory_graph.node_leaf import Node_Leaf
 from memory_graph.node_linear import Node_Linear
 from memory_graph.node_key_value import Node_Key_Value
+import memory_graph.config_helpers as config_helpers
 
 import memory_graph.utils as utils    
 import memory_graph.config as config
 
 import graphviz
+
 
 def read_nodes(data):
     """ Returns a dictionary that maps the each id found in 'data' to a Node,
@@ -50,19 +52,18 @@ def read_nodes(data):
     root_id = id(data)
     return nodes, root_id
 
-# --------------------------------------------------------------------------------------------
-
-def get_max_type_depth(node_id, node):
-    if node_id in config.type_to_depth:
-        return config.type_to_depth[node_id]
-    elif node.get_type() in config.type_to_depth:
-        return config.type_to_depth[node.get_type()]
-    return None
 
 def slice_nodes(nodes, root_id, max_graph_depth):
     """ Returns for nodes in nodes their slices that determines if and what part of
     a node is shown in the graph.
     """
+
+    def get_max_type_depth(node_id, node):
+        if node_id in config.type_to_depth:
+            return config.type_to_depth[node_id]
+        elif node.get_type() in config.type_to_depth:
+            return config.type_to_depth[node.get_type()]
+        return None
     
     def slice_nodes_recursive(nodes, node_id, id_to_slices, max_graph_depth):
         """ Recursively start at the root and slice all children until 'max_graph_depth'
@@ -90,105 +91,112 @@ def slice_nodes(nodes, root_id, max_graph_depth):
     slice_nodes_recursive(nodes, root_id, id_to_slices, max_graph_depth)
     return id_to_slices
 
-# --------------------------------------------------------------------------------------------
-
-def add_parent_indices(nodes, type_to_parent_indices, id_to_slices, max_missing_edges):
-    #print('add_parent_indices type_to_parent_indices:',type_to_parent_indices)
-    for _, parent_indices in type_to_parent_indices.items():
-        dashed = len(parent_indices) > max_missing_edges
-        for parent, index in parent_indices[0:max_missing_edges]:
-            new_parent = False
-            parent_id = parent.get_id()
-            if not parent_id in id_to_slices:
-                new_parent = True
-                id_to_slices[parent_id] = parent.get_children().empty_slices()
-            slices = id_to_slices[parent_id]
-            slices.add_index(index, dashed=dashed)
-            if new_parent:
-                add_indices_to_parents(nodes, parent_id, id_to_slices, max_missing_edges)
-
-def add_indices_to_parents(nodes, node_id, id_to_slices, max_missing_edges):
-    #print('add_indices_to_parents node_id:',node_id)
-    type_to_parent_indices = {}
-    parent_indices = nodes[node_id].get_parent_indices()
-    for parent, indices in parent_indices.items():
-        if parent is None:
-            continue
-        parent_type = parent.get_type()
-        parent_id = parent.get_id()
-        if (parent_type in type_to_parent_indices and 
-            len(type_to_parent_indices[parent_type]) > max_missing_edges): # early stop
-            continue
-        parent_slices = id_to_slices.get(parent_id, None)
-        for index in indices:
-            if parent_slices is None or not parent_slices.has_index(index):
-                if not parent_type in type_to_parent_indices:
-                    type_to_parent_indices[parent_type] = []
-                parent_indices = type_to_parent_indices[parent_type]
-                if len(parent_indices) > max_missing_edges:
-                    break
-                else:
-                    parent_indices.append((parent, index))
-    add_parent_indices(nodes, type_to_parent_indices, id_to_slices, max_missing_edges)
 
 def add_missing_edges(nodes, id_to_slices, max_missing_edges=3):
     """ Add missing edges to each visible node in the graph by addding slices to 'id_to_slices'.
     It either shows all edges to a node, or uses dashed edges to indicate some edges are missing.
     """
+    
+    def add_parent_indices(nodes, type_to_parent_indices, id_to_slices, max_missing_edges):
+        """ Add missing edges in 'type_to_parent_indices' to each parent. If the parent wasn't
+        visible before, recursively add missing edges to its parents.
+        """ 
+        #print('add_parent_indices type_to_parent_indices:',type_to_parent_indices)
+        for _, parent_indices in type_to_parent_indices.items():
+            dashed = len(parent_indices) > max_missing_edges
+            for parent, index in parent_indices[0:max_missing_edges]:
+                new_parent = False
+                parent_id = parent.get_id()
+                if not parent_id in id_to_slices:
+                    new_parent = True
+                    id_to_slices[parent_id] = parent.get_children().empty_slices()
+                slices = id_to_slices[parent_id]
+                slices.add_index(index, dashed=dashed)
+                if new_parent:
+                    add_indices_to_parents(nodes, parent_id, id_to_slices, max_missing_edges)
+
+    def add_indices_to_parents(nodes, node_id, id_to_slices, max_missing_edges):
+        """ Go over each parent of 'node_id', and add each missing index to each if it is
+        missing. Don't add more than 'max_missing_edges' for each type of parent to keep
+        graph small.
+        """
+        #print('add_indices_to_parents node_id:',node_id)
+        type_to_parent_indices = {}
+        parent_indices = nodes[node_id].get_parent_indices()
+        for parent, indices in parent_indices.items():
+            if parent is None:
+                continue
+            parent_type = parent.get_type()
+            parent_id = parent.get_id()
+            if (parent_type in type_to_parent_indices and 
+                len(type_to_parent_indices[parent_type]) > max_missing_edges): # early stop
+                continue
+            parent_slices = id_to_slices.get(parent_id, None)
+            for index in indices:
+                if parent_slices is None or not parent_slices.has_index(index):
+                    type_to_parent_indices.setdefault(parent_type, [])
+                    parent_indices = type_to_parent_indices[parent_type]
+                    if len(parent_indices) < max_missing_edges:
+                        parent_indices.append((parent, index))
+        add_parent_indices(nodes, type_to_parent_indices, id_to_slices, max_missing_edges)    
+    
     old_id_to_slices_keys = set(id_to_slices.keys())
     for node_id in old_id_to_slices_keys:
         add_indices_to_parents(nodes, node_id, id_to_slices, max_missing_edges)
     return id_to_slices
 
-# --------------------------------------------------------------------------------------------
-
-import memory_graph.config_helpers as config_helpers
-
-def create_depth_of_nodes(nodes, nodes_at_depth):
-    depth_of_nodes = {}
-    for node_id, depth in nodes_at_depth.items():
-        node = nodes[node_id]
-        if node_id in nodes and not node.is_hidden_node():
-            if not depth in depth_of_nodes:
-                depth_of_nodes[depth] = []
-            depth_of_nodes[depth].append(node)
-    return depth_of_nodes
-
-def add_subgraph(graphviz_graph, nodes_to_subgraph):
-    new_node_names = [node.get_name() for node in nodes_to_subgraph]
-    if len(new_node_names) > 1:
-        graphviz_graph.body.append('subgraph { rank=same; '+ ' -> '.join(new_node_names) + '[weight='+str(config.graph_stability)+', style=invis]; }\n')
-
-def add_to_graphviz_graph(graphviz_graph, nodes, node, slices, id_to_slices, subgraphed_nodes, depth):
-    html_table = node.get_html_table(nodes, slices, id_to_slices)
-    edges = html_table.get_edges()
-    color = config_helpers.get_color(node)
-    border = 3 if node.is_root() else 1
-    graphviz_graph.node(node.get_name(),
-                        html_table.to_string(border, color),
-                        xlabel=node.get_label(slices))
-    # ------------ edges
-    for parent,child,dashed in edges:
-        graphviz_graph.edge(parent, child+':table', style='dashed' if dashed else 'solid')
-
-def build_graph_depth_first(graphviz_graph, nodes,  node_id, id_to_slices, nodes_at_depth, subgraphed_nodes, depth):
-    if node_id in id_to_slices:
-        if node_id in nodes_at_depth:
-            return
-        nodes_at_depth[node_id] = depth
-        node = nodes[node_id]
-        children = node.get_children()
-        slices = None
-        if node_id in id_to_slices:
-            slices = id_to_slices[node_id]
-            if not slices is None:
-                for index in slices:
-                    child_id = id(children[index])
-                    build_graph_depth_first(graphviz_graph, nodes, child_id, id_to_slices, nodes_at_depth, subgraphed_nodes, depth+1)
-        if not node.is_hidden_node():
-            add_to_graphviz_graph(graphviz_graph, nodes, node, slices, id_to_slices, subgraphed_nodes, depth)
-
 def build_graph(graphviz_graph, nodes, root_id, id_to_slices):
+    """ Builds the graph of 'nodes' in 'graphviz_graph' starting at 'root_id' where 
+    'id_to_slices' indicates what is visible in the graph.
+    """
+
+    def create_depth_of_nodes(nodes, nodes_at_depth):
+        """ Creates a dictionary that maps each depth to the nodes at that depth. """
+        depth_of_nodes = {}
+        for node_id, depth in nodes_at_depth.items():
+            node = nodes[node_id]
+            if node_id in nodes and not node.is_hidden_node():
+                depth_of_nodes.setdefault(depth, [])
+                depth_of_nodes[depth].append(node)
+        return depth_of_nodes
+
+    def add_subgraph(graphviz_graph, nodes_to_subgraph):
+        """ Adds a subgraph to 'graphviz_graph' for 'nodes_to_subgraph'. """
+        new_node_names = [node.get_name() for node in nodes_to_subgraph]
+        if len(new_node_names) > 1:
+            graphviz_graph.body.append('subgraph { rank=same; '+ ' -> '.join(new_node_names) + '[weight='+str(config.graph_stability)+', style=invis]; }\n')
+
+    def add_to_graphviz_graph(graphviz_graph, nodes, node, slices, id_to_slices, subgraphed_nodes, depth):
+        """ Adds 'node' to 'graphviz_graph' with its children and edges. """
+        html_table = node.get_html_table(nodes, slices, id_to_slices)
+        edges = html_table.get_edges()
+        color = config_helpers.get_color(node)
+        border = 3 if node.is_root() else 1
+        graphviz_graph.node(node.get_name(),
+                            html_table.to_string(border, color),
+                            xlabel=node.get_label(slices))
+        # ------------ edges
+        for parent,child,dashed in edges:
+            graphviz_graph.edge(parent, child+':table', style='dashed' if dashed else 'solid')
+
+    def build_graph_depth_first(graphviz_graph, nodes,  node_id, id_to_slices, nodes_at_depth, subgraphed_nodes, depth):
+        """ Recursively builds the graph starting at 'node_id' and adds it to 'graphviz_graph'. """
+        if node_id in id_to_slices:
+            if node_id in nodes_at_depth:
+                return
+            nodes_at_depth[node_id] = depth
+            node = nodes[node_id]
+            children = node.get_children()
+            slices = None
+            if node_id in id_to_slices:
+                slices = id_to_slices[node_id]
+                if not slices is None:
+                    for index in slices:
+                        child_id = id(children[index])
+                        build_graph_depth_first(graphviz_graph, nodes, child_id, id_to_slices, nodes_at_depth, subgraphed_nodes, depth+1)
+            if not node.is_hidden_node():
+                add_to_graphviz_graph(graphviz_graph, nodes, node, slices, id_to_slices, subgraphed_nodes, depth)
+
     nodes_at_depth = {}
     build_graph_depth_first(graphviz_graph, nodes, root_id, id_to_slices, nodes_at_depth, set(), 0)
     depth_of_nodes = create_depth_of_nodes(nodes, nodes_at_depth)

commit 4e3bf7ce5b09c025fe3b398ad21409425fb55066

fixed hidden edges bug introduced in 108209c86dc50a5ed23c5d576f016fc31afc27b5

images/hidden_edges.py
@@ -2,7 +2,7 @@ import memory_graph as mg
 
 data = []
 x = ['x']
-for i in range(20):
+for i in range(35):
     data.append(x)
 
 mg.render(locals(), 'hidden_edges.png')

memory_graph/memory_to_nodes.py
@@ -149,8 +149,12 @@ def add_missing_edges(nodes, id_to_slices, max_missing_edges=3):
             for index in indices:
                 if parent_slices is None or not parent_slices.has_index(index):
                     type_to_parent_indices.setdefault(parent_type, [])
+                    #if not parent_type in type_to_parent_indices:
+                    #    type_to_parent_indices[parent_type] = []
                     parent_indices = type_to_parent_indices[parent_type]
-                    if len(parent_indices) < max_missing_edges:
+                    if len(parent_indices) > max_missing_edges:
+                        break
+                    else:
                         parent_indices.append((parent, index))
         add_parent_indices(nodes, type_to_parent_indices, id_to_slices, max_missing_edges)    
     