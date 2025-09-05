a9fdea4dfe1c2d7d675065b758cafb3b3a01601c
Bas Terwijn <bterwijn@gmail.com>
2025-09-05 15:58:41 +0200

    binary_convert.py added

---
+++ src/binary_convert.py
@@ -0,0 +1,11 @@
+import memory_graph as mg
+mg.config.type_to_vertical[list] = False  # horizontal lists
+
+def binary(value: int) -> list[int]:
+    if value == 0:
+        return []
+    quotient, remainder = divmod(value, 2)
+    result = binary(quotient) + [remainder]
+    return result
+
+print( binary(100) )
