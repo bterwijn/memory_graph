d5021d9d8cb2aceded2bd956fbc54580ef82c039
Bas Terwijn <bterwijn@gmail.com>
2025-10-19 17:32:22 +0200

    update

--- src/bitwise_operators.py
+++ src/bitwise_operators.py
@@ -4,13 +4,13 @@ class Bits(dict):
    """ Dictionary subclass that we will configure to show binary representations. """
 
 def twos_complement(x: int, bits: int) -> str:
-    """Return the two's-complement bit string of x in `bits` bits."""
+    """Return the two's complement bit string of x in `bits` bits."""
     mask = (1 << bits) - 1
     return format(x & mask, f"0{bits}b")
 
 # configure memory_graph to show binary representations of values of type Bits
 mg.config.type_to_node[Bits] = lambda x : mg.Node_Table(x,
-                                [ ["expression", "decimal", "bin(expression)", "16bit two's_complement"] ] +
+                                [ ["expression", "decimal", "bin(expression)", "16bit two's complement"] ] +
                                 [ [k,f'{v:>10}',f'{bin(v):>19}', twos_complement(v,16) ] for k,v in x.items()] )
 mg.config.type_to_slicer[Bits] = (mg.Slicer(), mg.Slicer())  # no slicing
 mg.config.type_to_color[Bits] = 'gold'
