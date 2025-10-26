532821813f89b9b95fcc935087302fdd711e87b4
Bas Terwijn <bterwijn@gmail.com>
2025-10-26 15:37:43 +0100

    update

---
+++ src/cocktail_sort.py
@@ -0,0 +1,42 @@
+import random
+
+mg.config.embedded_types -= {float}  # show separate nodes for floats
+
+def cocktail_shaker_sort(data):
+    n = len(data)
+    start = 0
+    end = n - 1
+    print('unserted section:',start,'through',end)
+    while True:
+        swapped = False
+        for i in range(start, end):
+            a, b = data[i], data[i + 1]
+            if a > b:
+                data[i], data[i + 1] = b, a
+                swapped = True
+                new_end = i
+        if not swapped:
+            break
+        end = new_end
+        print('unsorted:', data)
+        print('unsorted section:',start,'through',end)
+        swapped = False
+        for i in range(end, start, -1):
+            a, b = data[i], data[i - 1]
+            if a < b:
+                data[i], data[i - 1] = b, a
+                swapped = True
+                new_start = i
+        if not swapped:
+            break
+        start = new_start
+        print('unsorted:', data)
+        print('unsorted section:',start,'through',end)
+        
+        
+n = 10
+data = [float(i) for i in range(n)]
+random.shuffle(data)
+print('unsorted:', data)
+cocktail_shaker_sort(data)
+print('sorted:', data)
