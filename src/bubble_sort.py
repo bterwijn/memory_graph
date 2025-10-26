fd782afa11b851a85513a57cb45e80025968ce0d
Bas Terwijn <bterwijn@gmail.com>
2025-10-26 13:17:40 +0100

    update

---
+++ src/bubble_sort.py
@@ -0,0 +1,33 @@
+import random
+
+mg.config.embedded_types -= {float}  # show separate nodes for floats
+
+def bubble_sort_section(data, begini, endi):
+    swapped = False
+    last_swapi = 0
+    for i in range(begini, endi):
+        a = data[i]
+        b = data[i+1]
+        if a > b:
+            data[i], data[i+1] = b, a
+            if not swapped:
+                swapped = True
+                begini = max(i-1, 0)
+            last_swapi = i
+    return begini, last_swapi
+        
+def bubble_sort(data):
+    begini, endi = 0, len(data)-1
+    while True:
+        print('sorting section',begini,'through',endi)
+        begini, endi = bubble_sort_section(data, begini, endi)
+        if begini >= endi:
+            break
+        print('unsorted:', data) 
+
+n = 10
+data = [float(i) for i in range(n)]
+random.shuffle(data)
+print('unsorted:', data)
+bubble_sort(data)
+print('sorted:', data)
