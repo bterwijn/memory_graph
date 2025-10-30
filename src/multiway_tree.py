c005d5308c1667f09d57457f9c6f26664a244cbd
Bas Terwijn <bterwijn@gmail.com>
2025-10-30 22:51:13 +0100

    update

---
+++ src/multiway_tree.py
@@ -0,0 +1,33 @@
+import random
+
+mg.config.type_to_horizontal[list] = True # Visualize lists horizontally
+
+class Multi_Tree:
+    max_children = 3
+
+    def __init__(self):
+        self.values = []
+        self.children = []
+
+    def add_value(self, value):
+        if len(self.values) < self.max_children:
+            self.values.append(value)
+            self.values.sort()
+        else:
+            if not self.children:
+                for _ in range(self.max_children+1):
+                    self.children.append(Multi_Tree())
+            index = self._find_child_index(value)
+            self.children[index].add_value(value)
+
+    def _find_child_index(self, value):
+        for i, v in enumerate(self.values):
+            if value < v:
+                return i
+        return len(self.values)  # Go to the last child
+
+
+tree = Multi_Tree()
+n = 50
+for i in range(n):
+    tree.add_value(random.randint(1, n * 10))
