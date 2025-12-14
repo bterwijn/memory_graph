8c63897a0dfe24b6265c2d14c706bd5f13bac9dc
Bas Terwijn <bterwijn@gmail.com>
2025-12-14 15:25:26 +0100

    update

--- src/rebind.py
+++ src/rebind.py
@@ -3,6 +3,7 @@ b = a
 
 b += [1]        # changes the value of 'b' and 'a'
 b = [100, 200]  # rebinds 'b' to another value, 'a' is unaffected
-c = b + [300]   #   binds 'c' to new value 'b + [300]', `b` is unaffected
+c = b
+c = b + [300]   # rebinds 'c' to new value 'b + [300]', `b` is unaffected
 
 print(f'{a=}\n{b=}')
