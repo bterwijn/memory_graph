c033ab204533ef0f1136552ff21dfae461652dd2
Bas Terwijn <bterwijn@gmail.com>
2025-12-27 17:45:02 +0100

    update

--- src/hash_map.py
+++ src/hash_map.py
@@ -1,10 +1,14 @@
-import memory_graph as mg
 import random
 
 class Hash_Map:
+    INITIAL_SIZE = 5
+    REHASH_RATIO = 1.5
 
-    def __init__(self, size=25):
+    def __init__(self, size=None):
+        if not size:
+            size = Hash_Map.INITIAL_SIZE
         self.buckets = [[] for i in range(size)]
+        self.key_count = 0
 
     def _get_bucket(self, key):
         index = hash(key) % len(self.buckets)
@@ -17,6 +21,10 @@ class Hash_Map:
                 bucket[i] = (key, value)
                 return
         bucket.append((key, value))
+        self.key_count += 1
+        ratio = self.key_count / len(self.buckets)
+        if ratio > Hash_Map.REHASH_RATIO:
+            self.rehash()
 
     def __getitem__(self, key):
         bucket = self._get_bucket(key)
@@ -25,19 +33,55 @@ class Hash_Map:
                 return v
         raise KeyError(f"Key {key} not found")
 
+    def __delitem__(self, key):
+        bucket = self._get_bucket(key)
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                bucket.pop(i)
+                return
+        raise KeyError(f"Key {key} not found")
+
+    def rehash(self):
+        print("rehashing...")
+        new_size = len(self.buckets) * 2  # double size
+        new_buckets = [[] for i in range(new_size)]
+        for bucket in self.buckets:
+            for k, v in bucket:
+                index = hash(k) % new_size
+                new_buckets[index].append((k,v))
+        self.buckets = new_buckets
+        print("rehashing completed")
+
+    def keys(self):
+        all_keys = []
+        for bucket in self.buckets:
+            for k, v in bucket:
+                all_keys.append(k)
+        return all_keys
+
+print("=== create hash_map")
 hash_map = Hash_Map()
-n = 20
-value_range = n * 3 
-for i in range(n):
-    key = random.randint(1, value_range)
-    hash_map[f"key_{key}"] = f"value_{key}"
+n = 12
+value_range = n * 2
 
+print("=== add key-value pairs to hash_map, rehash when full")
 for i in range(n):
-    key = random.randint(1, value_range)
+    k = random.randint(1, value_range)
+    key, value = f"key_{k}", f"value_{k}"
+    print(f"adding {key} -> {value}")
+    hash_map[key] = value
+
+print("=== search for random keys in hash_map")
+for i in range(n):
+    key = f"key_{random.randint(1, value_range)}"
     try:
-        value = hash_map[f"key_{key}"]
-        print(f"Retrieved: key_{key} -> {value}")
+        value = hash_map[key]
+        print(f"Found: {key} -> {value}")
     except KeyError:
-        print(f"key_{key} not found in hash map")    
+        print(f"{key} not found in hash map")
 
-
+print("=== get each key from hash_map and delete it")
+all_keys = hash_map.keys()
+for key in all_keys:
+    print(f"delete {key}")
+    del hash_map[key]
