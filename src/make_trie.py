78b4067a2cd435cb13125eb9251af6f830b82b05
Bas Terwijn <bterwijn@gmail.com>
2025-12-30 12:33:03 +0100

    update

--- src/make_trie.py
+++ src/make_trie.py
@@ -1,7 +1,9 @@
+import random
+
+END = "_end_"
 
 def make_trie(words):
     root = {}
-    END = "_end_"
     for w in words:
         node = root
         for ch in w:
@@ -9,7 +11,30 @@ def make_trie(words):
         node[END] = True
     return root
 
+def complete_words(trie, start):
+    results = []
+
+    def dfs(node, prefix):
+        if END in node:
+            results.append(prefix)
+        for ch, child in node.items():
+            if ch != END:
+                dfs(child, prefix + ch)
+
+    node = trie
+    for ch in start:
+        if ch in node:
+            node = node[ch]
+        else:
+            return []
+    dfs(node, start)
+    return results
+    
 words = ["to", 
-         "too", "top", "toy", "ton", "toe", 
+         "too", "top", "toy", "ton", "toe", "tor", 
          "torn", "tore", "toss", "told", "toll", "tone"]
+random.shuffle(words)
 trie = make_trie(words)
+
+print( complete_words(trie, "to") )
+print( complete_words(trie, "tor") )
