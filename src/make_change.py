a1cee3ac7402de725159824fd3ad6be900e0ee46
Bas Terwijn <bterwijn@gmail.com>
2025-12-30 01:54:16 +0100

    update

--- src/make_change.py
+++ src/make_change.py
@@ -13,10 +13,16 @@ def make_change(amount, denominations):
         raise ValueError("Cannot make exact change with these denominations.")
     return change
 
-max_amount = 500
+def show_change(change):
+    print('coin  count')
+    for coin, count in change.items():
+        print(f'{coin:4}  {count:5}')
+
+max_amount = 1000
 amount = random.randrange(max_amount)
 print('amount:', amount)
 
 denominations = [50, 20, 10, 5, 2, 1]
 change = make_change(amount, denominations)
 print('change:', change)
+show_change(change)
