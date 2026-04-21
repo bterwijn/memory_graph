c48a0142e61505b2c688b3ff8e87d263e3879d3a
Bas Terwijn <bterwijn@gmail.com>
2026-04-21 19:43:22 +0200

    radix_sort.py updated

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,12 +1,11 @@
 import random
 
-def get_digit(i, d):
-    """ Get digit number 'd' from 'i' where 'd=0' is the right most digit. """
-    return (i // 10**d) % 10
-
 def counting_sort(new_data, data, digit):
+    """ Sorts `data` to the digit at position `digit` using stable 
+    Counting Sort. The sorted data is writen to `new_data`. """
     counts = [0] * 10
-    data_digits = [get_digit(i, digit) for i in data]
+    exp = 10 ** digit
+    data_digits = [(value // exp) % 10 for value in data]
     for d in data_digits:
         counts[d] += 1
     for i in range(1, len(counts)):
@@ -17,6 +16,8 @@ def counting_sort(new_data, data, digit):
         new_data[counts[d]] = data[i]
 
 def radix_sort(data, nr_digits):
+    """ Sorts the `data` using radix sort. The number of digits in the largest 
+    number is `nr_digits`. """
     new_data = data.copy()
     for d in range(nr_digits):
         counting_sort(new_data, data, d)
@@ -24,12 +25,13 @@ def radix_sort(data, nr_digits):
     return data
 
 def get_random_data(n, nr_digits):
+    """ Generates a list of `n` random integers with `nr_digits` digits. """
     range_end = 10**nr_digits
     return  [random.randrange(range_end) for _ in range(n)]
 
-n = 25
+n = 15
 nr_digits = 3
 data = get_random_data(n, nr_digits)
 print('data:', data)
-data_sorted = radix_sort(data, nr_digits)
-print('data_sorted:', data_sorted)
+data = radix_sort(data, nr_digits)
+print('data_sorted:', data)
