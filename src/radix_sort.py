d2f8fed7f8a8eb75a3720ff45f71eca541a2cd73
Bas Terwijn <bterwijn@gmail.com>
2026-04-24 00:48:27 +0200

    radix sort: recompute the digit, don't use memory to store it

--- src/radix_sort.py
+++ src/radix_sort.py
@@ -1,37 +1,37 @@
 import random
+BASE = 10
 
-def counting_sort(new_data, data, digit):
-    """ Sorts `data` to the digit at position `digit` using 
-    Counting Sort. The sorted data is writen to `new_data`. """
-    counts = [0] * 10
-    exp = 10 ** digit
-    data_digits = [(value // exp) % 10 for value in data]
-    for d in data_digits:
-        counts[d] += 1
+def counting_sort(data, data_new, digit):
+    """ Sorts `data` to the digit at position `digit` using stable
+    Counting Sort. The sorted data is writen to `data_new`. """
+    counts = [0] * BASE
+    exp = BASE ** digit
+    for d in data:
+        counts[(d // exp) % BASE] += 1
     for i in range(1, len(counts)):
         counts[i] += counts[i - 1]
     for i in range(len(data) - 1, -1, -1):
-        d = data_digits[i]
+        d = (data[i] // exp) % BASE
         counts[d] -= 1
-        new_data[counts[d]] = data[i]
+        data_new[counts[d]] = data[i]
 
 def radix_sort(data, nr_digits):
     """ Sorts `data` using radix sort. The number of digits in the largest 
     number is `nr_digits`. """
-    new_data = [0] * len(data)
+    data_new = [0] * len(data)
     for d in range(nr_digits):
-        counting_sort(new_data, data, d)
-        data, new_data = new_data, data
+        counting_sort(data, data_new, d)
+        data, data_new = data_new, data
     return data
 
 def get_random_data(n, nr_digits):
     """ Generates a list of `n` random integers with `nr_digits` digits. """
-    range_end = 10**nr_digits
+    range_end = BASE ** nr_digits
     return  [random.randrange(range_end) for _ in range(n)]
 
 n = 15
 nr_digits = 3
 data = get_random_data(n, nr_digits)
 print('data:', data)
-data = radix_sort(data, nr_digits)
-print('data_sorted:', data)
+data_sorted = radix_sort(data, nr_digits)  # corrupts 'data' 
+print('data_sorted:', data_sorted)
