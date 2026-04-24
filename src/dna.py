9cdb26d1599097e1238660125d9d81e49ecbc821
Bas Terwijn <bterwijn@gmail.com>
2026-04-24 13:30:10 +0200

    update

--- src/dna.py
+++ src/dna.py
@@ -1,15 +1,11 @@
 
 def find_kmer_positions(dna, k):
     positions = {}
-
     for i in range(len(dna) - k + 1):
         kmer = dna[i:i+k]
-
         if kmer not in positions:
             positions[kmer] = []
-
         positions[kmer].append(i)
-
     return positions
 
 
