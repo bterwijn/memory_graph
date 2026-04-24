8ea68527317dd726bab6e7a53668bb020e37ef5a
Bas Terwijn <bterwijn@gmail.com>
2026-04-24 13:21:43 +0200

    adding bioinformatics example

---
+++ src/dna.py
@@ -0,0 +1,21 @@
+
+def find_kmer_positions(dna, k):
+    positions = {}
+
+    for i in range(len(dna) - k + 1):
+        kmer = dna[i:i+k]
+
+        if kmer not in positions:
+            positions[kmer] = []
+
+        positions[kmer].append(i)
+
+    return positions
+
+
+dna = "ACGTTGCATGTCGCATGATGCATGAGAGCT"
+k = 4
+
+result = find_kmer_positions(dna, k)
+for kmer, positions in result.items():
+    print(f'{kmer}: {positions}')
